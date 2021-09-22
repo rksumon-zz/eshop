9622dd846b593acc921a979cbeac27fb5924f477
rksumon <sumon.npi@gmail.com>
2021-09-23 05:10:25 +0600

    app.js config

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,31 +1,49 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
 
 require('./bootstrap');
 
 window.Vue = require('vue').default;
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+//import vue-progressbar
+import VueProgressBar from 'vue-progressbar';
+Vue.use(VueProgressBar,options)
+const options = {
+    color: '#bffaf3',
+    failedColor: '#874b4b',
+    thickness: '5px',
+    transition: {
+        speed: '0.2s',
+        opacity: '0.6s',
+        termination: 300
+    },
+    autoRevert: true,
+    location: 'left',
+    inverse: false
+}
+// import sweetalert2
+import swal from 'sweetalert2'
+window.swal= swal;
+//config sweetalert2
+const toast = swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000
+})
+window.toast= toast;
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+// import vue router
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+let routes = [
+    {path: '/', component: require('./components/ExampleComponent').default},
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+]
+// vue router config
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
 
 const app = new Vue({
     el: '#app',
