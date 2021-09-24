bdb22552e7fe009087613c4723ed5632fc1ff428
rksumon <sumon.npi@gmail.com>
2021-09-25 01:30:04 +0600

    Admin supplier relationship database create with shop uid

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ProductCollection;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class ProductController extends Controller
+{
+    public function index(){
+        //return new ProductCollection(Product::orderBy('name','ASC')->where('user_id', '=', Auth::id())->paginate(10));
+        return new ProductCollection(Product::orderBy('name','ASC')->where('user_id', '=', 1)->paginate(10));
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('uid');
+            $table->string('shop_name');
+            $table->boolean('isAdmin')->default(true);
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
